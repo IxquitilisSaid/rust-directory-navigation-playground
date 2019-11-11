1a1fa89a60f47144466a71e4487828f3c87429a8
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2019-11-11 09:48:48 +0100

    midway with transversing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ extern crate error_chain;
 extern crate walkdir;
 extern crate dirs;
 
-// use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
+use std::path::PathBuf;
 
 error_chain! {
     foreign_links {
@@ -12,25 +13,42 @@ error_chain! {
     }
 }
 
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
 fn main() -> Result<()> {
-    // TODO: THIS BLOCK IS FOR MAPPING ALL HTML FILES
-    // for entry in WalkDir::new("../../")
-    //         .follow_links(true)
-    //         .into_iter()
-    //         .filter_map(|e| e.ok()) {
-
-    //     let f_name = entry.file_name().to_string_lossy();
-
-    //     if f_name.ends_with(".html") {
-    //         println!("{}", f_name);
-    //     }
-    // }
-
     // TODO: THIS GETS USER ROOT DIR
-    println!("{:#?}", dirs::home_dir());
+    let home = dirs::home_dir();
+    let home_dir = home.unwrap();
+    let taloom_dir: PathBuf = ["/Users/ixquitilis", "Documents/workRepos/taloom"].iter().collect();
+
+    // /Users/ixquitilis/Documents/workRepos/taloom/just-hire/src/main/angular2/src/app/view-applicant-rating-results/view-applicant-rating-results.component.html
+    // just-hire/src/main/angular2/src/app/view-applicant-rating-results/view-applicant-rating-results.component.html
+    println!("{:?}", home_dir);
 
     // TODO: THIS IS FOR TESTING WITH HOME DIR ABOVE
     // let path = std::fs::read_link("page_with_scrolling_frame.html")?;
+
+    // TODO: THIS BLOCK IS FOR MAPPING ALL HTML FILES
+    for entry in WalkDir::new(taloom_dir)
+            .follow_links(true)
+            .into_iter()
+            .filter_entry(|x| !is_hidden(x))
+            .filter_map(|e| e.ok()) {
+
+        let f_name = entry.file_name().to_string_lossy();
+        // let f_name = entry.path().display();
+
+        // println!("{}", f_name);
+
+        if f_name.ends_with(".html") {
+            println!("{}", f_name);
+        }
+    }
 
     // TODO: THIS BLOCK IS FOR READING THE FILE CONTENT
     // let content = std::fs::read_to_string("../../../Documents/workRepos/taloom/just-hire/src/main/angular2/src/index.html")
